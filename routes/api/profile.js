8335dbfec5bcab1440442024d90fc52340df1a99
Maaz Nonsola <maaznonsola786@gmail.com>
2024-01-21 20:31:44 +0530

    Create & Update Profile Routes (Comment Removed)

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -100,56 +100,6 @@ router.post(
       console.error(err.message);
       return res.status(500).send("Server Error");
     }
-
-    /* // destructure the request
-      const {
-        website,
-        skills,
-        youtube,
-        twitter,
-        instagram,
-        linkedin,
-        facebook,
-        // spread the rest of the fields we don't need to check
-        ...rest
-      } = req.body;
-  
-      // build a profile
-      const profileFields = {
-        user: req.user.id,
-        website:
-          website && website !== ''
-            ? normalize(website, { forceHttps: true })
-            : '',
-        skills: Array.isArray(skills)
-          ? skills
-          : skills.split(',').map((skill) => ' ' + skill.trim()),
-        ...rest
-      };
-  
-      // Build socialFields object
-      const socialFields = { youtube, twitter, instagram, linkedin, facebook };
-  
-      // normalize social fields to ensure valid url
-      for (const [key, value] of Object.entries(socialFields)) {
-        if (value && value.length > 0)
-          socialFields[key] = normalize(value, { forceHttps: true });
-      }
-      // add to profileFields
-      profileFields.social = socialFields;
-  
-      try {
-        // Using upsert option (creates new doc if no match is found):
-        let profile = await Profile.findOneAndUpdate(
-          { user: req.user.id },
-          { $set: profileFields },
-          { new: true, upsert: true, setDefaultsOnInsert: true }
-        );
-        return res.json(profile);
-      } catch (err) {
-        console.error(err.message);
-        return res.status(500).send('Server Error');
-      } */
   }
 );
 
