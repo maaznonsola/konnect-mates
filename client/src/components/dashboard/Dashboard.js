0e8703579e3da93c7ecadf2fdbafdbf6055e9b4e
Maaz Nonsola <maaznonsola786@gmail.com>
2024-03-17 12:43:03 +0530

    Showing Educations & Experiences on Dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,6 +4,8 @@ import PropTypes from "prop-types";
 import {connect} from "react-redux";
 import {getCurrentProfile} from "../../actions/profile";
 import DashboardActions from "./DashboardActions";
+import Experience from "./Experience";
+import Education from "./Education";
 
 const Dashboard = ({getCurrentProfile, auth: {user}, profile: {profile}}) => {
   useEffect(() => {
@@ -18,6 +20,8 @@ const Dashboard = ({getCurrentProfile, auth: {user}, profile: {profile}}) => {
       {profile !== null ? (
         <>
           <DashboardActions />
+          <Experience experience={profile.experience} />
+          <Education education={profile.education} />
           <div className="my-2">
             <button className="btn btn-danger" onClick={() => {}}>
               <i className="fas fa-user-minus" /> Delete My Account
