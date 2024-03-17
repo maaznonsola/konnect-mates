35c247fc6bb232587c5107fcb6f4b3e4b48f0b63
Maaz Nonsola <maaznonsola786@gmail.com>
2024-03-17 13:09:50 +0530

    Delete Education, Experience & Account

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,15 +2,20 @@ import React, {useEffect} from "react";
 import {Link} from "react-router-dom";
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
-import {getCurrentProfile} from "../../actions/profile";
+import {getCurrentProfile, deleteAccount} from "../../actions/profile";
 import DashboardActions from "./DashboardActions";
 import Experience from "./Experience";
 import Education from "./Education";
 
-const Dashboard = ({getCurrentProfile, auth: {user}, profile: {profile}}) => {
+const Dashboard = ({
+  getCurrentProfile,
+  deleteAccount,
+  auth: {user},
+  profile: {profile},
+}) => {
   useEffect(() => {
     getCurrentProfile();
-  }, []);
+  }, [getCurrentProfile]);
   return (
     <section className="container">
       <h1 className="large text-primary">Dashboard</h1>
@@ -23,7 +28,7 @@ const Dashboard = ({getCurrentProfile, auth: {user}, profile: {profile}}) => {
           <Experience experience={profile.experience} />
           <Education education={profile.education} />
           <div className="my-2">
-            <button className="btn btn-danger" onClick={() => {}}>
+            <button className="btn btn-danger" onClick={() => deleteAccount()}>
               <i className="fas fa-user-minus" /> Delete My Account
             </button>
           </div>
@@ -42,6 +47,7 @@ const Dashboard = ({getCurrentProfile, auth: {user}, profile: {profile}}) => {
 
 Dashboard.propTypes = {
   getCurrentProfile: PropTypes.func.isRequired,
+  deleteAccount: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
   profile: PropTypes.object.isRequired,
 };
@@ -51,4 +57,6 @@ const mapStateToProps = (state) => ({
   profile: state.profile,
 });
 
-export default connect(mapStateToProps, {getCurrentProfile})(Dashboard);
+export default connect(mapStateToProps, {getCurrentProfile, deleteAccount})(
+  Dashboard
+);
--- client/src/components/dashboard/Education.js
+++ client/src/components/dashboard/Education.js
@@ -1,9 +1,10 @@
 import React, {Fragment} from "react";
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
+import {deleteEducation} from "../../actions/profile";
 import formatDate from "../../utils/formatDate";
 
-const Education = ({education}) => {
+const Education = ({education, deleteEducation}) => {
   const educations = education.map((edu) => (
     <tr key={edu._id}>
       <td>{edu.school}</td>
@@ -12,7 +13,10 @@ const Education = ({education}) => {
         {formatDate(edu.from)} - {edu.to ? formatDate(edu.to) : "Now"}
       </td>
       <td>
-        <button onClick={() => {}} className="btn btn-danger">
+        <button
+          onClick={() => deleteEducation(edu._id)}
+          className="btn btn-danger"
+        >
           Delete
         </button>
       </td>
@@ -39,6 +43,7 @@ const Education = ({education}) => {
 
 Education.propTypes = {
   education: PropTypes.array.isRequired,
+  deleteEducation: PropTypes.func.isRequired,
 };
 
-export default Education;
+export default connect(null, {deleteEducation})(Education);
