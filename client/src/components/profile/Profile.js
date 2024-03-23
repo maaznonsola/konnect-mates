7762b65d388dcd718cfee3c12bd5fbf44bd3f232
Maaz Nonsola <maaznonsola786@gmail.com>
2024-03-23 14:58:54 +0530

    Displaying Github Repos

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -7,6 +7,7 @@ import ProfileTop from "./ProfileTop";
 import ProfileAbout from "./ProfileAbout";
 import ProfileExperience from "./ProfileExperience";
 import ProfileEducation from "./ProfileEducation";
+import ProfileGithub from "./ProfileGithub";
 import {getProfileById} from "../../actions/profile";
 
 const Profile = ({getProfileById, profile: {profile}, auth}) => {
@@ -33,6 +34,7 @@ const Profile = ({getProfileById, profile: {profile}, auth}) => {
           <div className="profile-grid my-1">
             <ProfileTop profile={profile} />
             <ProfileAbout profile={profile} />
+
             <div className="profile-exp bg-white p-2">
               <h2 className="text-primary">Experience</h2>
               {profile.experience.length > 0 ? (
@@ -64,6 +66,10 @@ const Profile = ({getProfileById, profile: {profile}, auth}) => {
                 <h4>No education credentials</h4>
               )}
             </div>
+
+            {profile.githubusername && (
+              <ProfileGithub username={profile.githubusername} />
+            )}
           </div>
         </Fragment>
       )}
