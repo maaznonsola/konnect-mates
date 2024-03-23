22ac5f4e039ee81b431492b82e7c0b7c86fd926f
Maaz Nonsola <maaznonsola786@gmail.com>
2024-03-23 14:00:00 +0530

    Starting on Profile

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,7 @@ import ProfileForm from "./components/profile-forms/ProfileForm";
 import AddExperience from "./components/profile-forms/AddExperience";
 import AddEducation from "./components/profile-forms/AddEducation";
 import Profiles from "./components/profiles/Profiles";
+import Profile from "./components/profile/Profile";
 import PrivateRoute from "./components/routing/PrivateRoute";
 
 // Redux
@@ -39,6 +40,7 @@ const App = () => {
           <Route path="register" element={<Register />} />
           <Route path="login" element={<Login />} />
           <Route path="profiles" element={<Profiles />} />
+          <Route path="profile/:id" element={<Profile />} />
           <Route
             path="dashboard"
             element={<PrivateRoute component={Dashboard} />}
