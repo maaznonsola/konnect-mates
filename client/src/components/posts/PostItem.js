30b57248348bf48a083566abd2cccedace443e5e
Maaz Nonsola <maaznonsola786@gmail.com>
2024-03-24 12:09:07 +0530

    Like & Unlike Functionality

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,8 +3,11 @@ import PropTypes from "prop-types";
 import {Link} from "react-router-dom";
 import formatDate from "../../utils/formatDate";
 import {connect} from "react-redux";
+import {addLike, removeLike} from "../../actions/post";
 
 const PostItem = ({
+  addLike,
+  removeLike,
   auth,
   post: {_id, text, name, avatar, user, likes, comments, date},
 }) => (
@@ -19,11 +22,19 @@ const PostItem = ({
       <p className="my-1">{text}</p>
       <p className="post-date">Posted on {formatDate(date)}</p>
 
-      <button onClick={() => {}} type="button" className="btn btn-light">
+      <button
+        onClick={() => addLike(_id)}
+        type="button"
+        className="btn btn-light"
+      >
         <i className="fas fa-thumbs-up" />{" "}
         <span>{likes.length > 0 && <span>{likes.length}</span>}</span>
       </button>
-      <button onClick={() => {}} type="button" className="btn btn-light">
+      <button
+        onClick={() => removeLike(_id)}
+        type="button"
+        className="btn btn-light"
+      >
         <i className="fas fa-thumbs-down" />
       </button>
       <Link to={`/posts/${_id}`} className="btn btn-primary">
@@ -44,10 +55,12 @@ const PostItem = ({
 PostItem.propTypes = {
   post: PropTypes.object.isRequired,
   auth: PropTypes.object.isRequired,
+  addLike: PropTypes.func.isRequired,
+  removeLike: PropTypes.func.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   auth: state.auth,
 });
 
-export default connect(mapStateToProps, {})(PostItem);
+export default connect(mapStateToProps, {addLike, removeLike})(PostItem);
